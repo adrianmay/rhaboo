96e0f0cea04c3ba518bbcbd41e4cb9db57b27f8b
Adrian May <adrian.alexander.may@gmail.com>
2014-08-11 16:11:32 +0800

    Better push and pop for arrays

--- arr.js
+++ arr.js
@@ -14,8 +14,11 @@ Array.prototype._rhaboo_originals = Array.prototype._rhaboo_originals || {
 //Very, very slow....
 Array.prototype._rhaboo_defensively = function(mutator) {
   return function () { 
-    var old = this.slice();
-    old._rhaboo = this._rhaboo;
+    var old;
+    if (this._rhaboo !== undefined) {
+      old = this.slice();
+      old._rhaboo = this._rhaboo;
+    }
     var retval = Array.prototype._rhaboo_originals[mutator].apply(this, arguments);
    // _rhaboo_trace("Overriding "+mutator+"... Old="+JSON.stringify(old));
     if (this._rhaboo !== undefined) {
@@ -26,8 +29,34 @@ Array.prototype._rhaboo_defensively = function(mutator) {
 }
 
 if (true) {
-  Array.prototype.push = Array.prototype._rhaboo_defensively("push");
-  Array.prototype.pop = Array.prototype._rhaboo_defensively("pop");
+  Array.prototype.push = function () {
+    var l1 = this.length;
+    var retval = Array.prototype._rhaboo_originals.push.apply(this, arguments);
+    var l2 = this.length;
+    if ( this._rhaboo !== undefined && l2>l1 ) {
+      for (var i=l1; i<l2; i++) {
+        var k = this._rhaboo_childKey(i);
+        enq( function(deferred) {
+          _rhaboo_stashers[_rhaboo_getTypeOf(this[i])](i, k, this[i], this);
+          deferred.resolve();
+        });
+      }
+    }
+  }
+  Array.prototype.pop = function () {
+    var l = this.length;
+    var retval = Array.prototype._rhaboo_originals.pop.apply(this, arguments);
+    if ( this._rhaboo !== undefined && l>0 ) {
+      var k = this._rhaboo_childKey(l-1);
+      enq( function(deferred) {
+        _rhaboo_forgetters[_rhaboo_getTypeOf(retval)](k, retval);
+        deferred.resolve();
+      });
+    } 
+    return retval;
+  }
+//  Array.prototype.push = Array.prototype._rhaboo_defensively("push");
+//  Array.prototype.pop = Array.prototype._rhaboo_defensively("pop");
   Array.prototype.shift = Array.prototype._rhaboo_defensively("shift");
   Array.prototype.unshift = Array.prototype._rhaboo_defensively("unshift");
   Array.prototype.splice = Array.prototype._rhaboo_defensively("splice");
