40316c741b2a5e260ef64407911a1c6c36354185
Adrian May <adrian.alexander.may@gmail.com>
2014-09-22 11:31:36 +0800

    firing q

--- src/enq.js
+++ src/enq.js
@@ -1,53 +1,53 @@
-    //Provides a sequential queue of background tasks using the q promises library
+//Provides a sequential queue of background tasks using the q promises library
 
-    var Q = require('q');
+var Q = require('q');
 
-    var enq_head = emptyPromise();
+var enq_head = emptyPromise();
 
-    function emptyPromise() {
-      var defer = new Q.defer();
-      defer.resolve();
-      return defer.promise;
+function emptyPromise() {
+  var defer = new Q.defer();
+  defer.resolve();
+  return defer.promise;
+}
+
+function enq (step) {
+  var f = function() {
+    var d = Q.defer();
+    step(d);
+    return d.promise;
+  }
+  enq_head = enq_head.then(f);
+  handleBusiness();
+}
+
+var businessPromise = undefined;
+var businessCallback = undefined;
+var businessReported = false;
+
+function handleBusiness() {
+  if (!businessCallback) {
+    return 0;
+  }
+  var busynow = enq_head.isPending();
+  if (busynow) {
+    businessPromise = enq_head.then(handleBusiness);
+  }
+  if (busynow != businessReported) {
+    businessReported = busynow;
+    if (businessCallback) {
+      businessCallback(businessReported); 
     }
+  }
+  return 0;
+}
 
-    function enq (step) {
-      var f = function() {
-        var d = Q.defer();
-        step(d);
-        return d.promise;
-      }
-      enq_head = enq_head.then(f);
-      handleBusiness();
-    }
+function onBusiness (callback) {
+//  businessCallback = callback;
+}
 
-    var businessPromise = undefined;
-    var businessCallback = undefined;
-    var businessReported = false;
-
-    function handleBusiness() {
-      if (!businessCallback) {
-        return 0;
-      }
-      var busynow = enq_head.isPending();
-      if (busynow) {
-        businessPromise = enq_head.then(handleBusiness);
-      }
-      if (busynow != businessReported) {
-        businessReported = busynow;
-        if (businessCallback) {
-          businessCallback(businessReported); 
-        }
-      }
-      return 0;
-    }
-
-    function onBusiness (callback) {
-      businessCallback = callback;
-    }
-
-    module.exports = {
-      enq:enq,
-      onBusiness:onBusiness
-    };
+module.exports = {
+  enq:enq,
+  onBusiness:onBusiness
+};
 
 
