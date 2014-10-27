8a69260ea77809c2d005eb6dfc156f652a7b4298
Adrian May <adrian.alexander.may@gmail.com>
2014-10-27 15:52:39 +0800

    Results

--- performance-tests/update-index.sh
+++ performance-tests/update-index.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
-echo "<html><body>" > index.htm
+(
+echo "<html><body><table border=2>" 
+echo "<tr><th rowspan=2>Filename<th colspan=3>Result<th rowspan=2>Units</tr>" 
+echo "<tr><th>Firefox<th>Chrome<th>IE</tr>" 
 for F in `ls -1 *.html` 
 do 
-  echo "<a href='$F'>$F</a><br/>" >> index.htm
+  echo "<tr>" 
+  echo "<td><a href='$F'>$F</a></td>" 
+  R=`grep ResultFirefox $F | sed 's/^.*://'`; echo "<td>$R</td>" 
+  R=`grep ResultChrome $F | sed 's/^.*://'`; echo "<td>$R</td>" 
+  R=`grep ResultIE $F | sed 's/^.*://'`; echo "<td>$R</td>" 
+  R=`grep ResultUnits $F | sed 's/^.*://'`; echo "<td>$R</td>" 
+  echo "</tr>" 
 done
-echo "</body></html>" >> index.htm
+echo "</table></body></html>" 
+) > index.htm
