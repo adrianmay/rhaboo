a93e5450c11bc6d75e00107e641590f924c03e4d
Adrian May <adrian.alexander.may@gmail.com>
2014-10-29 10:38:26 +0800

    Read test

--- performance/update-index.sh
+++ performance/update-index.sh
@@ -35,6 +35,7 @@ for F in `ls -1 p*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><t
 for F in `ls -1 sp*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><tr/>"
 for F in `ls -1 m*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><tr/>"
 for F in `ls -1 a*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><tr/>"
+for F in `ls -1 r*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><tr/>"
 for F in `ls -1 o*.html`; do line $F; done; echo "<tr><td colspan=5><hr/></td><tr/>"
 
 echo "</table>"
